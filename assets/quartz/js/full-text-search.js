d0e0462f912340cdba20ec85ca8ff71b5b4cb47f
secretsauce445 <maybemeidkisthisme@gmail.com>
2024-12-09 09:32:16 +0800

    Update full-text-search.js

--- assets/quartz/js/full-text-search.js
+++ assets/quartz/js/full-text-search.js
@@ -59,11 +59,70 @@
     displayResults(term, finalResults, true)
   })
 })();
-adsfas = document.getElementsByClassName("danger")
-
-for (i = 0; i < adsfas.length; i++) {
-    console.log(adsfas[i]);
-    adsfas[i].style.backgroundColor = "rgba(0, 137, 0, 0.3)";
+function switchTheme2(e) {
+    console.log("Changing modes...");
+    
+    if (e.target.checked) {
+        darkmode();
+    } else {
+        lightmode();
+    }
 }
 
+
+function darkmode() {
+    adsfas = document.getElementsByClassName("danger")
+        for (i = 0; i < adsfas.length; i++) {
+      adsfas[i].style.backgroundColor = "rgba(0, 137, 0, 0.3)";
+    }
+    
+    adsfas2 = document.getElementsByTagName("code")
+    for (i = 0; i < adsfas2.length; i++) {
+      adsfas2[i].style.backgroundColor = "rgb(43, 39, 94)";
+    }
+    
+    adsfas3 = document.getElementsByTagName("rect")
+    for (i = 0; i < adsfas3.length; i++) {
+      adsfas3[i].style.fill = "rgb(10, 30, 10)";
+    }
+    
+    adsfas4 = document.getElementsByClassName("nodeLabel")
+        for (i = 0; i < adsfas4.length; i++) {
+      adsfas4[i].style.color = "white";
+    }
+    
+    document.querySelector(".book-menu-content").style.backgroundColor = "rgb(27,26,26)";
+    document.querySelector("body > header").style.backgroundColor = "rgb(40,38,64)";
+}
+function lightmode() {
+    adsfas = document.getElementsByClassName("danger")
+    for (i = 0; i < adsfas.length; i++) {
+      adsfas[i].style.backgroundColor = "rgba(0, 137, 0, 0.3)";
+    }
+    
+    adsfas2 = document.getElementsByTagName("code")
+    for (i = 0; i < adsfas2.length; i++) {
+      adsfas2[i].style.backgroundColor = "rgb(222,214,197)";
+    }
+    
+    adsfas3 = document.getElementsByTagName("rect")
+    for (i = 0; i < adsfas3.length; i++) {
+      adsfas3[i].style.fill = "rgb(236, 236, 255)";
+    }
+    
+    adsfas4 = document.getElementsByClassName("nodeLabel")
+        for (i = 0; i < adsfas4.length; i++) {
+      adsfas4[i].style.color = "black";
+    }
+    document.querySelector(".book-menu-content").style.backgroundColor = "rgb(251,248,238)";
+    document.querySelector("body > header").style.backgroundColor = "rgb(46,141,83)";
+}
+
+
+document.querySelector("#darkmode-toggle").addEventListener("change", switchTheme2);
 document.querySelector("body > header").style.position = "fixed";
+if (document.querySelector("#darkmode-toggle").checked) {
+    darkmode();
+} else {
+    lightmode();
+}
